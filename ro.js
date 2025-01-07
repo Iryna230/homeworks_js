58fbe78917734af6e7aea3b1b852f5b8d133ae0f
Iryna Semykopenko <irynasemykopenko@gmail.com>
2025-01-07 17:00:01 +0100

    тестування функції ro

--- ro.js
+++ ro.js
@@ -12,11 +12,10 @@ function ro(x, y) {
     console.log(ro(2, 3)); // Виведе: 8
   
     
-    
     function ro(x, y) {
       // Ініціалізуємо результат
          let result = 1;
-         // Виконуємо цикл y разів
+         //Виконуємо цикл y разів
          for (let i = 0; i < y; i++) {
          result *= x; // Множимо result на x
          }
@@ -24,8 +23,8 @@ function ro(x, y) {
          return result;
          }
          // Приклад виклику функції
-         console.log(ro(10,1); // Виведе: 10
-         
+         console.log(ro(3, 2)); // Виведе: 9
+       
 
        
        
